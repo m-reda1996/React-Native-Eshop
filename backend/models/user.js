5bd954c64a13501cd0a431029c467294d53820f2
m-reda1996 <m.elshentenawy1996@gmail.com>
2023-01-10 01:15:12 +0200

    add users schema

---
+++ backend/models/user.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose")
+
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique : true
+  },
+  passwordHash: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+    unique : true
+    
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+  apartment: {
+    type: String,
+    default: "",
+  },
+  street: {
+    type: String,
+    default: "",
+  },
+  city: {
+    type: String,
+    default: "",
+  },
+})
+
+userSchema.virtual("id").get(function () {
+  return this._id.toHexString()
+})
+userSchema.set("toJSON", {
+  virtuals: true,
+})
+
+exports.Users = mongoose.model('User' , userSchema)
+
