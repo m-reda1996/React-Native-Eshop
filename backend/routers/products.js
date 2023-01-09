57d1d0ef1709db2f6c9a37c62d2be07b9d8d2f83
m-reda1996 <m.elshentenawy1996@gmail.com>
2023-01-09 22:02:32 +0200

    filtering product by ccategory && get products count

--- backend/routers/products.js
+++ backend/routers/products.js
@@ -2,17 +2,21 @@ const express = require("express")
 const { Category } = require("../models/category")
 const router = express.Router()
 const { Product } = require("../models/product")
-const mongoose = require('mongoose')
+const mongoose = require("mongoose")
 
 router.get("/", async (req, res) => {
-  const productList = await Product.find().populate('category') //.select('name image price category')
+  let filter = {}
+  if (req.query.categories) {
+    filter = { category: req.query.categories.split(",") }
+  }
+  const productList = await Product.find(filter).populate("category") //.select('name image price category')
   res.send(productList)
 })
-router.get('/:id', async (req,res) =>{
-  const {id} = req.params
-  if(!mongoose.isValidObjectId(id)) return res.status(400).send('invalid product id ')
-  const product = await Product.findById(id).populate('category')
-  if(!product) return res.status(500).json({message : 'the product with the given ID was not found' })
+router.get("/:id", async (req, res) => {
+  const { id } = req.params
+  if (!mongoose.isValidObjectId(id)) return res.status(400).send("invalid product id ")
+  const product = await Product.findById(id).populate("category")
+  if (!product) return res.status(500).json({ message: "the product with the given ID was not found" })
   res.send(product)
 })
 
@@ -42,25 +46,44 @@ router.post("/", async (req, res) => {
   res.send(product)
 })
 
-router.put('/:id' , async(req,res) => {
-  const {id} = req.params
-  if(!mongoose.isValidObjectId(id)) return res.status(400).send('invalid product id ')
+router.put("/:id", async (req, res) => {
+  const { id } = req.params
+  if (!mongoose.isValidObjectId(id)) return res.status(400).send("invalid product id ")
   const productCategory = await Category.findById(req.body.category)
   if (!productCategory) return res.status(400).send("invalid category")
   const { name, discription, richDescription, image, images, brand, price, category, countInStock, rating, numReviews, isFeatured, dateCreated } = req.body
-  const product = await Product.findByIdAndUpdate(id,{name, discription, richDescription, image, images, brand, price, category, countInStock, rating, numReviews, isFeatured, dateCreated},{new : true})
+  const product = await Product.findByIdAndUpdate(
+    id,
+    { name, discription, richDescription, image, images, brand, price, category, countInStock, rating, numReviews, isFeatured, dateCreated },
+    { new: true }
+  )
   if (!product) {
-      res.status(500).json({ message : 'the categoy with the given ID was not found' })
-    } 
+    res.status(500).json({ message: "the categoy with the given ID was not found" })
+  }
   res.send(product)
 })
 
-
 router.delete("/:id", async (req, res) => {
   const { id } = req.params
-  if(!mongoose.isValidObjectId(id)) return res.status(400).send('invalid product id ')
+  if (!mongoose.isValidObjectId(id)) return res.status(400).send("invalid product id ")
   const product = await Product.findByIdAndDelete(id)
   res.send("delted")
+})
+
+router.get("/get/count", async (req, res) => {
+  const productCount = await Product.countDocuments("name")
+  if (!productCount) return res.status(500).json({ message: "the product with the given ID was not found" })
+  res.send({
+    productCount: productCount,
+  })
+})
+
+router.get("/get/featured/:count", async (req, res) => {
+  let count = req.params.count ? req.params.count : 1
+
+  const product = await Product.find({ isFeatured: true }).limit(+count)
+  if (!product) return res.status(500).json({ message: "the product with the given ID was not found" })
+  res.send(product)
 })
 
 module.exports = router
