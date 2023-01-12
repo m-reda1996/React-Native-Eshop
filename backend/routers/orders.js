a77459e89732077bd07baaf9570e8488c40ed028
m-reda1996 <m.elshentenawy1996@gmail.com>
2023-01-13 01:56:36 +0200

    add user orders and total pice for order delete item when delete order

--- backend/routers/orders.js
+++ backend/routers/orders.js
@@ -6,6 +6,11 @@ const mongoose = require("mongoose")
 
 router.get(`/`, async (req, res) => {
   const orderList = await Order.find()
+    .populate({ path: "orderItems", populate: { path: "product", populate: "category" } })
+  .populate("user", "name")
+  .sort("dateOrder")
+  .select("totalPrice")
+
   if (!orderList) {
     res.status(500).json({ success: false })
   }
@@ -15,6 +20,8 @@ router.get("/:id", async (req, res) => {
   const { id } = req.params
   if (!mongoose.isValidObjectId(id)) return res.status(400).send("invalid order id ")
   const order = await Order.findById(id)
+    .populate({ path: "orderItems", populate: { path: "product", populate: "category" } })
+    .populate("user", "name")
   if (!order) {
     res.status(500).json({ message: "the categoy with the given ID was not found" })
   }
@@ -32,8 +39,16 @@ router.post("/", async (req, res) => {
     })
   )
   const orderItem = await orderItemsIds
-  console.log(orderItem)
-  const { shippingAddress1, shippingAddress2, city, phone, status, totalPrice, user, dateOrder } = req.body
+  const { shippingAddress1, shippingAddress2, city, phone, status, user, dateOrder } = req.body
+  const totalPrices = await Promise.all(
+    orderItem.map(async (orderItemId) => {
+      const orderItem = await OrderItem.findById(orderItemId).populate("product", "price")
+      const totalPrice = orderItem.product.price * orderItem.quantity
+      return totalPrice
+    })
+  )
+  const totalPrice = totalPrices.reduce((a, b) => a + b, 0)
+  console.log(totalPrices)
   let order = new Order({
     orderItems: orderItem,
     shippingAddress1,
@@ -41,7 +56,7 @@ router.post("/", async (req, res) => {
     city,
     phone,
     status,
-    totalPrice,
+    totalPrice: totalPrice,
     user,
     dateOrder,
   })
@@ -63,12 +78,12 @@ router.put("/:id", async (req, res) => {
 })
 
 router.delete("/:id", (req, res) => {
-
   const { id } = req.params
   if (!mongoose.isValidObjectId(id)) return res.status(400).send("invalid category id ")
-    Order.findByIdAndDelete(id).then(async order => {
+  Order.findByIdAndDelete(id)
+    .then(async (order) => {
       if (order) {
-        await order.orderItems.map( async orderItem =>{
+        await order.orderItems.map(async (orderItem) => {
           await OrderItem.findByIdAndDelete(orderItem)
         })
         return res.status(200).json({ success: true, message: "deleted" })
@@ -80,7 +95,33 @@ router.delete("/:id", (req, res) => {
       return res.status(400).json({ success: false, error: err })
     })
 })
+router.get("/get/totalsales", async (req, res) => {
+  const totalSales = await Order.aggregate([{ $group: { _id: null, totalSales: { $sum: "$totalPrice" } } }])
+  if (!totalSales) {
+    return res.status(4000).send("the order salws connat be generated")
+  }
+  res.send({ totalSales: totalSales.pop().totalSales })
+})
 
+router.get("/get/count", async (req, res) => {
+  const orderCount = await Order.countDocuments("name")
+  if (!orderCount) return res.status(500).json({ message: "the order with the given ID was not found" })
+  res.send({
+    orderCount: orderCount,
+  })
+})
+
+router.get("/get/userorders/:userId", async (req, res) => {
+  const { userId } = req.params
+  const userOrderList = await Order.find({user : userId})
+    .populate({ path: "orderItems", populate: { path: "product", populate: "category" } })
+    .sort({"dateOrder" : -1})
+
+    if(!userOrderList){
+      res.status(500).json({success : false})
+    }
+    res.send(userOrderList)
+})
 // router.delete("/:id", async (req, res) => {
 
 //   const { id } = req.params
@@ -88,6 +129,5 @@ router.delete("/:id", (req, res) => {
 //   const order = await Order.findByIdAndDelete(id)
 //   res.send("delted")
 // })
-
 
 module.exports = router
